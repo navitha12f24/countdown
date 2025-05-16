cdb95701974962be37de2a5247991ea58b4eec27
navitha12f24 <navitha12f24@gmail.com>
2025-05-16 14:27:05 +0530

    first commit

---
+++ code.js
@@ -0,0 +1,33 @@
+setTimeout(()=>{
+    document.getElementById("a1").innerHTML=10
+    setTimeout(()=>{
+    document.getElementById("a1").innerHTML=9
+    setTimeout(()=>{
+    document.getElementById("a1").innerHTML=8
+    setTimeout(()=>{
+    document.getElementById("a1").innerHTML=7
+    setTimeout(()=>{
+    document.getElementById("a1").innerHTML=6
+    setTimeout(()=>{
+    document.getElementById("a1").innerHTML=5
+    setTimeout(()=>{
+    document.getElementById("a1").innerHTML=4
+    setTimeout(()=>{
+    document.getElementById("a1").innerHTML=3
+    setTimeout(()=>{
+    document.getElementById("a1").innerHTML=2
+    setTimeout(()=>{
+    document.getElementById("a1").innerHTML=1
+    setTimeout(()=>{
+    document.getElementById("a1").innerHTML="sparkles"
+},1000)   
+},1000)   
+},1000)   
+},1000)   
+},1000)   
+},1000)   
+},1000)   
+},1000)   
+},1000)   
+},1000)   
+},1000)                                                                                       
