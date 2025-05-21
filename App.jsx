56442ac9fb8c6ff3e3d18ca343b0b4af53461ed5
navitha12f24 <navitha12f24@gmail.com>
2025-05-21 10:48:23 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,18 @@
+
+import Sample from "./Components/sample"
+
+function App() {
+  const myName = "navitha"
+  const Age="19"
+  const hobby="drawing"
+  return (
+    <>
+    <h1>My React App</h1>
+    <h1 className=" text-30x1 font-bold underline text-pink-600">Hello world</h1>
+    <Sample myName={myName} Age={Age} hobby={hobby}/>
+    </>
+  )
+}
+
+
+export default App
